c3c568e59e7e667e0e6246f0b8b98d4cc9a26f75
jssjssj <148733552+jssjssj@users.noreply.github.com>
2023-12-04 17:50:35 +0900

    [김지산] scheduleController 일부추가

--- diary/src/main/java/com/example/diary/controller/ScheduleController.java
+++ diary/src/main/java/com/example/diary/controller/ScheduleController.java
@@ -2,11 +2,21 @@ package com.example.diary.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
 
 import com.example.diary.service.ScheduleService;
+import com.example.diary.vo.Schedule;
+
+import jakarta.servlet.http.HttpSession;
 
 @Controller
 public class ScheduleController {
+	@Autowired ScheduleService scheduleService;
 	
-	
+	@GetMapping("/scheduleByDay")
+	public String scheduleByDay(Schedule schedule , HttpSession session , Integer targetYear, Integer targetMonth
+									) {
+		
+		return "schedule/scheduleByDay";
+	}
 }
