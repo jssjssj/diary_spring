b47691d26f3a7d6f43c78bc26090bd6c0a2a424b
jssjssj <148733552+jssjssj@users.noreply.github.com>
2024-01-30 15:08:27 +0900

    fix: DiaryApplication 수정
    	- 에러페이지 접근 시 조회화면 조정

--- diary/src/main/java/com/example/diary/DiaryApplication.java
+++ diary/src/main/java/com/example/diary/DiaryApplication.java
@@ -1,15 +1,40 @@
 package com.example.diary;
 
+import jakarta.servlet.http.HttpServletRequest;
+import java.util.NoSuchElementException;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.ServletComponentScan;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.EnableAspectJAutoProxy;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 
+@ControllerAdvice(annotations = Controller.class)
 @SpringBootApplication
-@ServletComponentScan
+@ComponentScan(basePackages = { "com.example", "com.example.gym.controller" })
+@EnableScheduling
+@EnableAspectJAutoProxy
 public class DiaryApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(DiaryApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(DiaryApplication.class, args);
+    }
 
+    @ExceptionHandler(Exception.class)
+    public String handleExceptions(
+        Exception ex,
+        Model model,
+        HttpServletRequest request
+    ) {
+        if (ex instanceof NoSuchElementException) {
+            model.addAttribute("errorMessage", "올바르지 않은 접근입니다.");
+            return "error/error";
+        } else {
+            model.addAttribute("errorMessage", "올바르지 않은 접근입니다.");
+            return "error/error";
+        }
+    }
 }
